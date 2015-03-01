3a0c14b94a8e83ebbdf28b0b28849327d031ff25
J David Smith <emallson@archlinux.us>
2015-02-28 19:09:50 -0500

    Added some sql changes from last meeting.

--- src/sql/create_feedback_table.sql
+++ src/sql/create_feedback_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE feedback (
   id integer PRIMARY KEY,
   user_id integer REFERENCES user (id),
   datapoint_id integer REFERENCES datapoint (id),
-  dataset_id integer REFERENCES dataset (id),
-  CONSTRAINT u_constraint UNIQUE (user_id, datapoint_id, dataset_id)
+  subject_id integer REFERENCES subject (id),
+  CONSTRAINT u_constraint UNIQUE (user_id, datapoint_id, subject_id)
 );
--- src/sql/create_subject_table.sql
+++ src/sql/create_subject_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE subjects (
 	id        integer PRIMARY KEY DEFAULT nextval('serial'),
 	ds_id     integer REFERENCES dataset (id),
-	targets   text[],
+	target    text,
 	dummies   text[]
 );
