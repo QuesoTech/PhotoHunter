b9782723d6d3e73859243e42d16776c001bd29a4
Connor Greenwell <cgree3@gmail.com>
2015-04-11 11:53:26 -0400

    dont commit creds, yo

--- src/researcherinterface/rif.go
+++ src/researcherinterface/rif.go
@@ -3,18 +3,17 @@
 package main
 
 import (
-	
-	"net/http"
 	"database/sql"
 	"fmt"
 	"log"
-	_"github.com/gorilla/sessions"
-	
+	"net/http"
+	"os"
+
+	_ "github.com/gorilla/sessions"
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
-
 
 func check(err error) {
 	if err != nil {
@@ -23,34 +22,33 @@ func check(err error) {
 }
 
 func getDbHandle() (db *sql.DB, err error) {
-	user := "tester"
-	password := "test"
-	host:= "localhost"
-	dbname:= "mydb"
+	// credentials and sensitive information are stred in the
+	// environment so that we dont have to keep a config file that might
+	// accidentally get committed...
+	user := os.Getenv("$DBUSER")
+	password := os.Getenv("$DBPASSWORD")
+	host := os.Getenv("$DBHOST")
+	dbname := os.Getenv("$DBNAME")
 
-	conn_string := fmt.Sprintf("postgres://%s:%s@%s/%s",user,password,host,dbname)
-	db,err = sql.Open("postgres", conn_string)
+	conn_string := fmt.Sprintf("postgres://%s:%s@%s/%s", user, password, host, dbname)
+	db, err = sql.Open("postgres", conn_string)
 	return
 }
 
-
 func main() {
-	
+
 	var err error
 	//Database handlers
-	DB,err = getDbHandle()
+	DB, err = getDbHandle()
 	check(err)
 	defer DB.Close()
 	check(DB.Ping())
-	
-
 
 	//Templates and specific pages
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/account",accountHandler)
-	http.HandleFunc("/signup",signupHandler)
-	http.HandleFunc("/signin",signinHandler)
-
+	http.HandleFunc("/account", accountHandler)
+	http.HandleFunc("/signup", signupHandler)
+	http.HandleFunc("/signin", signinHandler)
 
 	//Static file servers
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
