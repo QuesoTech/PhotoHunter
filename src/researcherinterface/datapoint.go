449a5df6a4b2e2bc65341b8605ca341c8c71bf71
Connor Greenwell <cgree3@gmail.com>
2015-04-11 13:11:30 -0400

    more datapoint code

--- src/researcherinterface/datapoint.go
+++ src/researcherinterface/datapoint.go
@@ -1,12 +1,32 @@
 package main
 
 type Datapoint struct {
-	Id        int
-	DatasetId int
+	Id        int64
+	DatasetId int64
 	ImageURL  string
 }
 
-func GetDatapointById(id int) (dp *Datapoint, err error) {
+func NewDatapoint(datasetId int64, imageURL string) (dp *Datapoint, err error) {
+	stmt, err := DB.Prepare("INSERT INTO datapoint (dataset_id, image_url) VALUES ($1, $2)")
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(datasetId, imageURL)
+	if err != nil {
+		return
+	}
+
+	id, err := res.LastInsertId()
+	dp.Id = id
+	dp.DatasetId = datasetId
+	dp.ImageURL = imageURL
+
+	return
+}
+
+func GetDatapointById(id int64) (dp *Datapoint, err error) {
 	stmt, err := DB.Prepare("SELECT id, dataset_id, image_url FROM datapoint WHERE id = $1")
 	if err != nil {
 		return
@@ -15,7 +35,7 @@ func GetDatapointById(id int) (dp *Datapoint, err error) {
 
 	row := stmt.QueryRow(id)
 
-	var datasetID int
+	var datasetID int64
 	var imageURL string
 
 	err = row.Scan(&id)
