916299e69a8f38ca39b5130e67f967e84d3e301e
Aaron Bradshaw <aarontravisbradshaw@gmail.com>
2015-04-11 11:19:41 -0400

    changing the researcher sql

--- src/sql/create_researcher_table.sql
+++ src/sql/create_researcher_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE researchers (
-	id        integer PRIMARY KEY DEFAULT nextval('serial'),
+	id        serial PRIMARY KEY,
 	fname     varchar(20) NOT NULL,
 	lname     varchar(25) NOT NULL,
-	email     varchar(50) NOT NULL		 
+	email     varchar(50) UNIQUE NOT NULL		 
 
 );
