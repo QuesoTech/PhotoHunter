a0abd56d9c0a239a57ce2277a5b67cd8d9d29908
Connor Greenwell <cgree3@gmail.com>
2015-04-13 13:53:43 -0400

    fixing datapoint, we should probably move away from preparing statements since were not reusing them

--- src/researcherinterface/datapoint.go
+++ src/researcherinterface/datapoint.go
@@ -7,21 +7,15 @@ type Datapoint struct {
 }
 
 func NewDatapoint(datasetId int64, imageURL string) (dp *Datapoint, err error) {
-	stmt, err := DB.Prepare("INSERT INTO datapoint (dataset_id, image_url) VALUES ($1, $2)")
-	if err != nil {
-		return
-	}
-	defer stmt.Close()
-
 	var id int64
-	err := stmt.QueryRow(datasetId, imageURL).Scan(&id)
+	err = DB.QueryRow("INSERT INTO datapoint (dataset_id, image_url) VALUES ($1, $2) RETURNING id", datasetId, imageURL).Scan(&id)
 	if err != nil {
 		return
 	}
 
 	dp = &Datapoint{
 		Id:        id,
-		DatasetID: datasetId,
+		DatasetId: datasetId,
 		ImageURL:  imageURL,
 	}
 
@@ -29,20 +23,10 @@ func NewDatapoint(datasetId int64, imageURL string) (dp *Datapoint, err error) {
 }
 
 func GetDatapointById(id int64) (dp *Datapoint, err error) {
-	stmt, err := DB.Prepare("SELECT id, dataset_id, image_url FROM datapoint WHERE id = $1")
-	if err != nil {
-		return
-	}
-	defer stmt.Close()
-
-	row := stmt.QueryRow(id)
-
 	var datasetID int64
 	var imageURL string
 
-	err = row.Scan(&id)
-	err = row.Scan(&datasetID)
-	err = row.Scan(&imageURL)
+	err = DB.QueryRow("SELECT id, dataset_id, image_url FROM datapoint WHERE id=$1", id).Scan(&id, &datasetID, &imageURL)
 
 	if err != nil {
 		return
