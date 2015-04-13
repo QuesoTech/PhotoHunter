89a627b2329dcb7acdabe0de23100346520e4027
Connor Greenwell <cgree3@gmail.com>
2015-04-13 14:15:42 -0400

    feedback init and getter

--- src/researcherinterface/feedback.go
+++ src/researcherinterface/feedback.go
@@ -1,8 +1,44 @@
 package main
 
 type Feedback struct {
-	Id            int
-	UserId        int
-	Datapoint     int
-	RequirementId int
+	Id            int64
+	UserId        int64
+	DatapointId   int64
+	RequirementId int64
+}
+
+func NewFeedback(u_id, dp_id, req_id int64) (fb *Feedback, err error) {
+	var id int64
+	err = DB.QueryRow("INSERT INTO feedback (user_id, datapoint_id, requirement_id) VALUES ($1, $2, $3) RETURNING id", u_id, dp_id, req_id).Scan(&id)
+	if err != nil {
+		return
+	}
+
+	fb = &Feedback{
+		Id:            id,
+		UserId:        u_id,
+		DatapointId:   dp_id,
+		RequirementId: req_id,
+	}
+
+	return
+}
+
+func GetFeedbackById(id int64) (fb *Feedback, err error) {
+	var u_id int64
+	var dp_id int64
+	var req_id int64
+	err = DB.QueryRow("SELECT user_id, datapoint_id, requirement_id FROM feedback WHERE id=$1", id).Scan(&u_id, &dp_id, &req_id)
+	if err != nil {
+		return
+	}
+
+	fb = &Feedback{
+		Id:            id,
+		UserId:        u_id,
+		DatapointId:   dp_id,
+		RequirementId: req_id,
+	}
+
+	return
 }
