79e3fc8487245aedfcaec62027cf346f3c896c1c
Connor Greenwell <cgree3@gmail.com>
2015-04-18 13:15:42 -0400

    subject creation

--- src/researcherinterface/subject.go
+++ src/researcherinterface/subject.go
@@ -3,8 +3,24 @@ package main
 // Subject represnts the subject being captured by a dataset. A dataset can have
 // many of these
 type Subject struct {
-	Id        int
-	DatasetId int
+	ID        int64
+	DatasetID int64
 	Target    string
 	Dummies   []string
+}
+
+func NewSubject(databaseID int64, target string, dummies []string) (s *Subject, err error) {
+	var id int64
+
+	err := DB.QueryRow("INSERT INTO subject (ds_id, target, dummies) VALUES ($1, $2, $3) RETURNING id", databaseID, target, dummies).Scan(&id)
+	if err != nil {
+		return
+	}
+
+	s = &Subject{
+		ID:        id,
+		DatasetID: datasetID,
+		Target:    target,
+		Dummies:   dummies,
+	}
 }
