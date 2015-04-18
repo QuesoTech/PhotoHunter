6ef6fc8dba39e7bbe807568b170e0320ae978f25
Aaron Bradshaw <aarontravisbradshaw@gmail.com>
2015-04-18 13:42:54 -0400

    Working on creating datasets

--- src/researcherinterface/handlers.go
+++ src/researcherinterface/handlers.go
@@ -7,6 +7,9 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
+	"strconv"
+	"time"
 )
 
 var store = sessions.NewCookieStore([]byte("aaron"))
@@ -45,6 +48,49 @@ func singleHandler(path string, fname string) {
 	})
 }
 
+//Creates a dataset
+func createDatasetHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session-name")
+	r.ParseForm()
+	var id int64
+
+
+	if rid, ok := session.Values["id"].(string); ok {
+		id,_ = strconv.ParseInt(rid,10,64)
+	}
+	
+	//create dataset
+	
+	name := r.FormValue("name")
+
+	//create subjects
+	target := r.FormValue("target")
+	dummies := strings.Split(r.FormValue("dummies"),",")
+
+	//create locations
+	lat,err := strconv.ParseFloat(r.FormValue("lat"),64)
+	long,err := strconv.ParseFloat(r.FormValue("long"),64)
+	
+	stime,err := time.Parse("15:04:05",r.FormValue("stime"))
+	etime,err := time.Parse("15:04:05",r.FormValue("etime"))
+	
+	//time of day. HH:MM:SS
+	
+	ds, err := NewDataset(id, name)
+	_,err = NewSubject(ds.ID,target,dummies)
+	_,err = NewLocation(ds.ID,GeoPoint{Lat:lat,Lon:long})
+	_,err = NewTimeperiod(ds.ID,stime,etime)
+
+	if err != nil {
+	http.Redirect(w,r,"/",http.StatusFound)
+	}else {
+		http.Redirect(w,r,"/",http.StatusNotFound)
+	}
+	
+	//POINT(lat,long)
+}
+
+
 //Gets the data submitted from sign up. Encrypts the password and
 //creates a new user account.
 func signupHandler(w http.ResponseWriter, r *http.Request) {
@@ -74,8 +120,9 @@ func signinHandler(w http.ResponseWriter, r *http.Request) {
 	email := string(r.FormValue("email"))
 	pword := []byte(r.FormValue("pword"))
 	var hword []byte
-
-	err := DB.QueryRow("SELECT pword FROM researchers WHERE email=$1", email).Scan(&hword)
+	var id int64
+	var fname, lname string
+	err := DB.QueryRow("SELECT pword,id,fname,lname FROM researchers WHERE email=$1", email).Scan(&hword,&id,&fname,&lname)
 
 	switch {
 	case err == sql.ErrNoRows:
@@ -87,7 +134,13 @@ func signinHandler(w http.ResponseWriter, r *http.Request) {
 		check(err)
 
 		session, _ := store.Get(r, "session-name")
+
+		
+
 		session.Values["user"] = email
+		session.Values["fname"] = fname
+		session.Values["lname"] = lname
+		session.Values["id"] = id
 		session.Save(r, w)
 		http.Redirect(w, r, "/", http.StatusFound)
 	}
