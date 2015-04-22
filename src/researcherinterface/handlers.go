7c7958512bb9eaf17856687203f8c8780af2dcef
Aaron Bradshaw <aarontravisbradshaw@gmail.com>
2015-04-22 10:40:14 -0400

    Fixed Hotel California bug with researcher interface

--- src/researcherinterface/handlers.go
+++ src/researcherinterface/handlers.go
@@ -178,7 +178,12 @@ func signinHandler(w http.ResponseWriter, r *http.Request) {
 
 func logoutHandler(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "session-name")
-	delete(session.Values, "user")
+	//delete(session.Values, "user")
+
+	delete(session.Values,"email")
+	delete(session.Values,"fname")
+	delete(session.Values,"lname")
+	delete(session.Values,"id")
 	session.Save(r, w)
 
 	http.Redirect(w, r, "/", http.StatusFound)
