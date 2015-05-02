e7fa2dc81395c2f8637f4ea25b74dc63cab0cef2
Connor Greenwell <cgree3@gmail.com>
2015-05-02 13:44:38 -0400

    added skeletons for api call handles

---
+++ src/researcherinterface/photohunter.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http"
+)
+
+// getDatasetsHandler handles requests from photohunter for lists of available datasets
+func getDatasetsHandler(w http.ResponseWriter, r *http.Request) {}
+
+// submitDatapointHandler handles requests from photohunter for submitting data to a dataset
+func submitDataHandler(w http.ResponseWriter, r *http.Request) {}
